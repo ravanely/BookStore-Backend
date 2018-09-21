ead7c8dec5fc0bf0b0050590c8460e5cb167a11f
Nague <nagueravanely@gmail.com>
2018-09-21 08:25:52 +0100

    ajout d une fonction qui retourne le token et d une fonction de deconnexion

--- src/main/java/cm/bookstore/resource/LoginResource.java
+++ src/main/java/cm/bookstore/resource/LoginResource.java
@@ -7,7 +7,11 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import cm.bookstore.service.UserService;
@@ -29,5 +33,16 @@ public class LoginResource {
 		System.out.println(request.getRemoteAddr());
 		
 		return Collections.singletonMap("token", session.getId());
+	}
+	
+	@RequestMapping("/checkSession")
+	public ResponseEntity checkSession() {
+		return new ResponseEntity("Session Active !", HttpStatus.OK);
+	}
+	
+	@RequestMapping(value="/user/logout", method=RequestMethod.POST)
+	public ResponseEntity logout() {
+		SecurityContextHolder.clearContext();
+		return new ResponseEntity("Logout Sucessfully!", HttpStatus.OK);
 	}
 }
